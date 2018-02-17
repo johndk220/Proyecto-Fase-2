ca43c09fcb9337556199a8952e7315d7e4890129
johndk220 <34284684+johndk220@users.noreply.github.com>
2018-02-16 23:28:08 -0400

    Update Page6.php

--- public_html/Proyecto-Final/Parts/Volleyball/Page6.php
+++ public_html/Proyecto-Final/Parts/Volleyball/Page6.php
@@ -1,11 +1,11 @@
 
 <nav aria-label="Page navigation example">
-  <ul class="pagination">
-  <li class="page-item"><a class="page-link" href="index.php?a=Page5">&laquo;</a></li>
-    <li class="page-item"><a class="page-link" href="index.php?a=Page5"> Colocacion o Acomodo </a></li>
-    <li class="page-item"><li class="page-item active"><a class="page-link" href="index.php?a=Page6">Ataque </a></li>
-    <li class="page-item"><a class="page-link" href="index.php?a=Page7"> Video como jugar volleyball</a></li>
-    <li class="page-item"><a class="page-link" href="index.php?a=Page7">&raquo;</a></li>
+ <ul class="pagination">
+  <li class="page-item"><a class="page-link" href="index.php?a=volley&p=5">&laquo;</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?a=volley&p=5"> Colocacion o Acomodo </a></li>
+    <li class="page-item"><li class="page-item active"><a class="page-link" href="index.php?a=volley&p=6">Ataque </a></li>
+    <li class="page-item"><a class="page-link" href="index.php?a=volley&p=7"> Video como jugar volleyball</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?a=volley&p=7">&raquo;</a></li>
   </ul>
 </nav>
 <center><img src="https://image.slidesharecdn.com/impress-copia2-160124132309/95/impress-copia-2-15-638.jpg?cb=1453641866" alt="Ataque" width="386" height="235"></center>
