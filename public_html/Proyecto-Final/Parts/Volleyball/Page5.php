842cc255414af0e81e2d03836f3a41aeb4cea044
johndk220 <34284684+johndk220@users.noreply.github.com>
2018-02-16 23:27:08 -0400

    Update Page5.php

--- public_html/Proyecto-Final/Parts/Volleyball/Page5.php
+++ public_html/Proyecto-Final/Parts/Volleyball/Page5.php
@@ -1,11 +1,11 @@
 
 <nav aria-label="Page navigation example">
-  <ul class="pagination">
-   <li class="page-item"><a class="page-link" href="index.php?a=Page4">&laquo;</a></li>
-    <li class="page-item"><a class="page-link" href="index.php?a=Page4">Recepcion</a></li>
-    <li class="page-item"><li class="page-item active"><a class="page-link" href="index.php?a=Page5"> Colocacion o Acomodo </a></li>
-    <li class="page-item"><a class="page-link" href="index.php?a=Page6">Ataque </a></li>
-    <li class="page-item"><a class="page-link" href="index.php?a=Page6">&raquo;</a></li>
+<ul class="pagination">
+   <li class="page-item"><a class="page-link" href="index.php?a=volley&p=4">&laquo;</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?a=volley&p=4">Recepcion</a></li>
+    <li class="page-item"><li class="page-item active"><a class="page-link" href="index.php?a=volley&p=5"> Colocacion o Acomodo </a></li>
+    <li class="page-item"><a class="page-link" href="index.php?a=volley&p=6">Ataque </a></li>
+    <li class="page-item"><a class="page-link" href="index.php?a=volley&p=6">&raquo;</a></li>
   </ul>
 </nav>
 <h2> Colocacion o Acomodo </h2>
