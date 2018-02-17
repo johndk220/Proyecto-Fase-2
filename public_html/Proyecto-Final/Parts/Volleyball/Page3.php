a040b7b1804a4dc4d4f80dbfa4e3a97eb2597497
johndk220 <34284684+johndk220@users.noreply.github.com>
2018-02-16 23:23:41 -0400

    Update Page3.php

--- public_html/Proyecto-Final/Parts/Volleyball/Page3.php
+++ public_html/Proyecto-Final/Parts/Volleyball/Page3.php
@@ -1,11 +1,11 @@
 
 <nav aria-label="Page navigation example">
-  <ul class="pagination">
-<li class="page-item"><a class="page-link" href="index.php?a=Page2">&laquo;</a></li>
-    <li class="page-item"><a class="page-link" href="index.php?a=Page2">Servicio</a></li>
-    <li class="page-item"><li class="page-item active"><a class="page-link" href="index.php?a=Page3">Bloqueo</a></li>
-    <li class="page-item"><a class="page-link" href="index.php?a=Page4">Recepcion</a></li>
-    <li class="page-item"><a class="page-link" href="index.php?a=Page4">&raquo;</a></li>
+<ul class="pagination">
+<li class="page-item"><a class="page-link" href="index.php?a=volley&p=2">&laquo;</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?a=volley&p=2">Servicio</a></li>
+    <li class="page-item"><li class="page-item active"><a class="page-link" href="index.php?a=volley&p=3">Bloqueo</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?a=volley&p=4">Recepcion</a></li>
+    <li class="page-item"><a class="page-link" href="index.php?a=volley&p=4">&raquo;</a></li>
   </ul>
 </nav>
 <center><img src="https://1.bp.blogspot.com/-ZetiZWJN3RA/WORJTGJ93UI/AAAAAAAAFrw/nog6_bz8A64xMZsYiAvygmPKb-cpGi7ygCEw/s1600/Bloqueo%2B2.JPG" alt="Bloqueo" width="386" height="235"></center>
